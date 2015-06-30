3de835b9ad5b6d9f0fe22c9a9f7c9033282012b2
Peter <asimplepeter@gmail.com>
2015-06-30 14:46:28 -0400

    added some npm dependencies

--- server/api/thing/thing.socket.js
+++ server/api/thing/thing.socket.js
@@ -30,6 +30,8 @@ setInterval(function(){
 
 }, 500);
 
+// ref https://github.com/aivis/user-stream
+
 var Stream = require('user-stream');
 var stream = new Stream({
     consumer_key: 'DSeQTW6yqDyhfcKurO8cum1Ag',
@@ -47,6 +49,33 @@ stream.on('data', function(json) {
 });
 
 }
+
+// ref https://github.com/ciaranj/node-oauth
+
+describe('OAuth1.0',function(){
+  var OAuth = require('oauth');
+
+  it('tests trends Twitter API v1.1',function(done){
+    var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      'DSeQTW6yqDyhfcKurO8cum1Ag',
+      'DzrJxwCKurEdENCiTL7r8K7fEbFP1rusbVX5ogtisANlL0kIqG',
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );
+    oauth.get(
+      'https://api.twitter.com/1.1/trends/place.json?id=23424977',
+      'your user token for this app', //test user token
+      'your user secret for this app', //test user secret
+      function (e, data, res){
+        if (e) console.error(e);
+        console.log(require('util').inspect(data));
+        done();
+      });
+  });
+});
 
 function onSave(socket, doc, cb) {
   // name of event to send back to the browser

f64f72b3f04e44d8f2278b983499d076951f861d
Matt <matvaldez@gmail.com>
2015-07-01 11:48:05 -0400

    added abiltiy to render teams model on main.html

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,26 @@
 
 angular.module('project3App')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.baseballTeams = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/teams').success(function(baseballTeams) {
+      $scope.baseballTeams = baseballTeams;
+      socket.syncUpdates('team', $scope.baseballTeams);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addTeam = function() {
+      if($scope.newTeam === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/teams', { name: $scope.newTeam });
+      $scope.newTeam = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteTeam = function(team) {
+      $http.delete('/api/teams/' + team._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('team');
     });
   });
